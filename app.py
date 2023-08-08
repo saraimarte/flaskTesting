c39f5defb6bd2330d7bbf85afde9f3569ca90155
Sarai Marte <smartepujols@gmail.com>
2023-08-08 14:03:16 -0400

    Info from backend used in frontend

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-
-a = False
-
-@app.route("/")
-def home():
-    return "Home Page"
-
+   
 @app.route("/<name>")
-def user(name):
-    return f'Hello {name}!'
-
-@app.route("/admin")
-def admin():
-    if a:
-        return "You are an admin!"
-    else:
-        return redirect(url_for("user", name = "Admin!"))
-    
+def home(name):
+    return render_template("index.html", content = name)
 
 if __name__ == "__main__":
     app.run()
