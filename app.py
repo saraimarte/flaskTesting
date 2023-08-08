79513f6c12f83bb813d05cc2f61d1fc55405be41
Sarai Marte <smartepujols@gmail.com>
2023-08-08 13:54:28 -0400

    If certain condition is true then redirect 2a page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
+
+a = False
 
 @app.route("/")
 def home():
@@ -9,6 +11,14 @@ def home():
 @app.route("/<name>")
 def user(name):
     return f'Hello {name}!'
+
+@app.route("/admin")
+def admin():
+    if a:
+        return "You are an admin!"
+    else:
+        return redirect(url_for("user", name = "Admin!"))
+    
 
 if __name__ == "__main__":
     app.run()
