7bdb3ffd4d4c3520c38f0d65dcd31380c9968713
Sarai Marte <smartepujols@gmail.com>
2023-08-08 14:50:51 -0400

    Flasks version of components and layouts

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
    
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content = ['tim', 'joe', 'bill'])
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
