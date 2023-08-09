f108bbaee41498403d966cd399f1674bf20ee83b
Sarai Marte <smartepujols@gmail.com>
2023-08-09 16:29:23 -0400

    c

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -15,9 +15,11 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session['user'] = user
+        flash(f"You {user} have been logged in!", "info")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash(f"You are already logged in!")
             return redirect(url_for("user"))
         else:
            return render_template("login.html")
@@ -26,13 +28,17 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return render_template("user.html", user = user)
     else:
         return redirect(url_for("login"))
     
 @app.route("/logout")
 def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
-if __name__ == "__main__":
+     if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user} !", "info")
+     session.pop("user", None)
+     return redirect(url_for("login"))
+
+if __name__ == "__main__": 
     app.run(debug = True)
