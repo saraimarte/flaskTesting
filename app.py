39722c83650d8fd09c04f2570fde075f41fd6f0c
Sarai Marte <smartepujols@gmail.com>
2023-08-09 00:27:28 -0400

    permanent sessions

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
-
 app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes = 5)
 
 @app.route("/")
 def home():
@@ -11,6 +12,7 @@ def home():
 @app.route("/login", methods = ["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
         session['user'] = user
         return redirect(url_for("user"))
