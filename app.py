bbf81272fbb8877e4f3d9c6c0e1fdfc0f775707f
Sarai Marte <smartepujols@gmail.com>
2023-08-09 00:23:01 -0400

    Login and Logout Sessions Tut 5

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
-   
+
+app.secret_key = "hello"
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -10,13 +12,25 @@ def home():
 def login():
     if request.method == "POST":
         user = request.form["nm"]
-        return redirect(url_for("user", usr = user))
+        session['user'] = user
+        return redirect(url_for("user"))
     else:
-        return render_template("login.html")
+        if "user" in session:
+            return redirect(url_for("user"))
+        else:
+           return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
-
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 if __name__ == "__main__":
     app.run(debug = True)
